55d72345802e9adb7a1bbe424dedf910712f4669
shurikperunov@mail.ru <shurikperunov@mail.ru>
2022-09-16 00:11:59 +0500

    fix: динамически обновляем год

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 function Footer() {
+  let date = new Date();
+
   return (
     <footer className="footer">
-      <p className="footer__text">&copy; 2022 Mesto Russia</p>
+      <p className="footer__text">&copy; {date.getFullYear()} Mesto Russia</p>
     </footer>
   );
 }
